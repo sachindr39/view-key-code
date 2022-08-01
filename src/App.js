ea58a0e037873a4a703f229a7e71d0918cf15996
Sachin Raut <sachindr39@gmail.com>
2022-08-01 20:12:43 +0530

    Initial release

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const [number, setNumber] = useState('');
+  const [color, setColor] = useState(22);
+
+  const type = (event) => {
+    setNumber(event.keyCode)
+    setColor(Math.floor(Math.random() * 1200))
+    console.log(color)
+  }
+
+  useEffect(() => {
+    document.addEventListener('keypress', type)
+    return () => document.removeEventListener('keypress', type)
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      {
+        number ? (
+          <>
+            <h2
+            style={{
+              background: 'hsl('+color+'deg 56% 47%)'
+            }}
+            >{number}</h2>
+            <p>Your pressed key code is above.</p>
+          </>
+        ) : (
+          <p>Please press any key to see key code.</p>
+        )
+      }
+    </>
+  )
 }
 
 export default App;
